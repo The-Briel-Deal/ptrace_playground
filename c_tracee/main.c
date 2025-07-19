11f9ed25aa7551f2604d1717da7a5dd23716a95f
Gabriel Ford <gabe@fordltc.net>
2025-07-19 12:21:10 -0400

    add gitignore, wait for attach

--- c_tracee/main.c
+++ c_tracee/main.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
+#include <sys/ptrace.h>
+#include <unistd.h>
+
+volatile int gdb_attached = 0;
 
 int main() {
-	int a = 2;
-	int b = 8;
-	int c = a + b;
+  while (gdb_attached == 0)
+    sleep(1);
+  int a = 2;
+  int b = 8;
+  int c = a + b;
   printf("a = %d, b = %d, c = %d\n", a, b, c);
   return 0;
 }

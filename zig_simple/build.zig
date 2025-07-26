065c8dace20d13e65bffce15274e402000a9fa5a
Gabriel Ford <gabe@fordltc.net>
2025-07-26 12:39:48 -0400

    Change options to create minimal binary size

--- zig_simple/build.zig
+++ zig_simple/build.zig
@@ -2,22 +2,26 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
-
-    const mod = b.addModule("zig_simple", .{
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-    });
+    // const optimize = b.standardOptimizeOption(.{ .preferred_optimize_mode = .ReleaseSmall });
 
     const exe = b.addExecutable(.{
         .name = "zig_simple",
         .root_module = b.createModule(.{
             .root_source_file = b.path("src/main.zig"),
             .target = target,
-            .optimize = optimize,
-            .imports = &.{
-                .{ .name = "zig_simple", .module = mod },
-            },
+            .optimize = .ReleaseSmall,
+            .code_model = .small,
+            .error_tracing = true,
+            .link_libc = false,
+            .no_builtin = true,
+            .single_threaded = true,
+            .sanitize_c = .off,
+            .stack_check = false,
+            .unwind_tables = .none,
+            .valgrind = false,
+            .stack_protector = false,
+            .link_libcpp = false,
+            .strip = true,
         }),
     });
 
@@ -33,20 +37,4 @@ pub fn build(b: *std.Build) void {
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
-
-    const mod_tests = b.addTest(.{
-        .root_module = mod,
-    });
-
-    const run_mod_tests = b.addRunArtifact(mod_tests);
-
-    const exe_tests = b.addTest(.{
-        .root_module = exe.root_module,
-    });
-
-    const run_exe_tests = b.addRunArtifact(exe_tests);
-
-    const test_step = b.step("test", "Run tests");
-    test_step.dependOn(&run_mod_tests.step);
-    test_step.dependOn(&run_exe_tests.step);
 }
